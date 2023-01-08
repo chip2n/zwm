4073ea94078cc7205fdd4207c4efb641aa874f89
chip2n <andreas@arvidsson.io>
2023-01-08 15:44:54 +0100

    Add getGeometry function

--- src/util.zig
+++ src/util.zig
@@ -1,13 +1,45 @@
 const std = @import("std");
 const c = @import("c.zig");
 
-/// Check X11 status for success
+/// Check X11 status for success.
 pub fn check(status: c.Status) !void {
     if (status == 0) {
         return error.XlibError;
     }
 }
 
+const XGeometry = struct {
+    root: c.Window,
+    x: c_int,
+    y: c_int,
+    width: c_uint,
+    height: c_uint,
+    border: c_uint,
+    depth: c_uint,
+};
+
+/// Returns the root window and the current geometry of the drawable.
+pub fn getGeometry(display: *c.Display, d: c.Drawable) XGeometry {
+    var root: c.Window = undefined;
+    var x: c_int = undefined;
+    var y: c_int = undefined;
+    var width: c_uint = undefined;
+    var height: c_uint = undefined;
+    var border_width: c_uint = undefined;
+    var depth: c_uint = undefined;
+
+    _ = c.XGetGeometry(display, d, &root, &x, &y, &width, &height, &border_width, &depth);
+
+    return .{
+        .root = root,
+        .x = x,
+        .y = y,
+        .width = width,
+        .height = height,
+        .border = border_width,
+        .depth = depth,
+    };
+}
 // /// Get unqualified type name for provided XEvent type
 // pub fn eventTypeName(comptime Event: type) []const u8 {
 //     var split = std.mem.splitBackwards(u8, @typeName(Event), ".");
